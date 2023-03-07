74a0cd9a50ca708259f41fcdbeed2841026a30f7
thai2062001 <phamthai180@gmail.com>
2023-03-07 22:15:59 +0700

    update home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,58 +1,101 @@
 import classNames from 'classnames/bind';
 import styles from './Home.module.scss'
-import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Card, CardContent, Typography, Button } from '@material-ui/core';
-
+import { FaAngleDoubleRight, FaAngleDoubleLeft } from 'react-icons/fa';
+import { useEffect,useRef } from 'react';
 const cx = classNames.bind(styles)
 
 
 function Home() {
-    const useStyles = makeStyles((theme) => ({
-        root: {
-          flexGrow: 1,
-          padding: theme.spacing(4),
-        },
-        card: {
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          minHeight: 200,
-          backgroundColor: theme.palette.primary.main,
-          color: theme.palette.primary.contrastText,
-        },
-        title: {
-          marginBottom: theme.spacing(2),
-        },
-        button: {
-          marginTop: theme.spacing(2),
-        },
-      }));
-      
-      const BodyComponent = () => {
-        const classes = useStyles();
 
-        
-    return ( 
-        <div className={cx('wrapper')}>
-            <div className={cx('content')} >
-                <div className={cx('banner')}>
-                    <h1>
-                        <span>Find</span> your job
-                    </h1>
-                    <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                     typesetting industry. Lorem Ipsum has been the industry's
-                      standard dummy text ever since the 1500s,
-                       when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
-                        but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-                        sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    </p>
-                </div>
+
+  const HandleNext = ()=>{
+      let lists = document.querySelectorAll('#item');
+      document.getElementById('slide').appendChild(lists[0]);
+   }
+
+   const HandlePrev = ()=>{
+    let lists = document.querySelectorAll('#item');
+    document.getElementById('slide').prepend(lists[lists.length -1]);
+ }
+
+  return (
+    <div className={cx('container_full')}> 
+<div className={cx('container')}>
+  <div className={cx('title_slide')}>
+    <h1>Các công ty nổi bật</h1>
+  </div>
+      <div className={cx('wrapper')}>
+        <div id='slide' className={cx('banner')}>
+          <div id='item' className={cx('item')} style={{ background: 'rgb(100,132,187)', backgroundImage: 'linear-gradient(90deg, rgba(100,132,187,1) 18%, rgba(100,132,187,1) 35%, rgba(100,132,187,1) 50%, rgba(64,161,211,1) 68%, rgba(48,174,222,1) 76%, rgba(0,212,255,1) 100%)' }}>
+            <div className={cx('content')}>
+              <div className={cx('from')}>FPT</div>
+              <div className={cx('name')}>Công ty FPT</div>
+              <div className={cx('type')}>IT</div>
             </div>
+          </div>
+
+          <div id='item' className={cx('item')} style={{ background: 'rgb(100,132,187)', backgroundImage: 'linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)' }}>
+            <div className={cx('content')}>
+              <div className={cx('from')}>Viettel</div>
+              <div className={cx('name')}>Công ty Viettel</div>
+              <div className={cx('type')}>IT</div>
+            </div>
+          </div>
+
+          <div id='item' className={cx('item')} style={{ background: 'rgb(100,132,187)', backgroundImage: ' radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)' }}>
+            <div className={cx('content')}>
+              <div className={cx('from')}>Viettel</div>
+              <div className={cx('name')}>Công ty Viettel</div>
+              <div className={cx('type')}>IT</div>
+            </div>
+          </div>
+          <div id='item' className={cx('item')} style={{ background: 'rgb(100,132,187)', backgroundImage: ' radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)' }}>
+            <div className={cx('content')}>
+              <div className={cx('from')}>Viettel</div>
+              <div className={cx('name')}>Công ty Viettel</div>
+              <div className={cx('type')}>IT</div>
+            </div>
+          </div>
+          <div id='item' className={cx('item')} style={{ background: 'rgb(100,132,187)', backgroundImage: ' radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,231) 100%)' }}>
+            <div className={cx('content')}>
+              <div className={cx('from')}>Viettel</div>
+              <div className={cx('name')}>Công ty Viettel</div>
+              <div className={cx('type')}>IT</div>
+            </div>
+
+          </div>
+          <div id='item' className={cx('item')} style={{ background: 'rgb(100,132,187)', backgroundImage: ' radial-gradient(circle, rgba(174,202,1) 0%, rgba(148,187,233,1) 100%)' }}>
+            <div className={cx('content')}>
+              <div className={cx('from')}>Viettel</div>
+              <div className={cx('name')}>Công ty Viettel</div>
+              <div className={cx('type')}>IT</div>
+            </div>
+          </div>
+
+        </div>
+
+        <div className={cx('buttons')}>
+          <button className={cx('prev')} onClick={HandlePrev} id='prev'>
+          <FaAngleDoubleLeft/>
+          </button>
+          <button onClick={HandleNext} id='next'>
+            <FaAngleDoubleRight/>
+          </button>
+        </div>
+
       </div>
-    );
+
+    </div>
+    <div className={cx('introduce')}>
+    <div className={cx('introduce-banner')}>
+      <h1>Give me your money</h1>
+    </div>
+    </div>
+
+    </div>
     
+  );
+
 }
-}
+
 export default Home;
