cca5e3a74945de25ecd998535f2c40bf5d4d52fd
thai2062001 <phamthai180@gmail.com>
2023-03-08 17:48:04 +0700

    home css

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,6 +3,10 @@ import styles from './Home.module.scss'
 import { FaAngleDoubleRight, FaAngleDoubleLeft } from 'react-icons/fa';
 import { useEffect,useState } from 'react';
 import jwt_decode from "jwt-decode";
+import bannerimages from '../images/banner.PNG'
+import viettel from '../images/viettel.jpg'
+const viettel1 = 'https://thanhtra.com.vn/data/images/0/2021/01/07/congdinh/logo-moi-cua-viettel.jpg?dpi=150&quality=100&w=630&mode=crop&anchor=topcenter&scale=both'
+
 
 
 const cx = classNames.bind(styles)
@@ -50,18 +54,18 @@ function Home() {
   </div>
       <div className={cx('wrapper')}>
         <div id='slide' className={cx('banner')}>
-          <div id='item' className={cx('item')} style={{ background: 'rgb(100,132,187)', backgroundImage: 'linear-gradient(90deg, rgba(100,132,187,1) 18%, rgba(100,132,187,1) 35%, rgba(100,132,187,1) 50%, rgba(64,161,211,1) 68%, rgba(48,174,222,1) 76%, rgba(0,212,255,1) 100%)' }}>
+          <div id='item' className={cx('item','Viettel')}  >
             <div className={cx('content')}>
-              <div className={cx('from')}>FPT</div>
-              <div className={cx('name')}>Công ty FPT</div>
+              <div className={cx('from')}>Viettel</div>
+              <div className={cx('name')}>Công ty viễn thông Viettel</div>
               <div className={cx('type')}>IT</div>
             </div>
           </div>
 
           <div id='item' className={cx('item')} style={{ background: 'rgb(100,132,187)', backgroundImage: 'linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)' }}>
             <div className={cx('content')}>
-              <div className={cx('from')}>Viettel</div>
-              <div className={cx('name')}>Công ty Viettel</div>
+              <div className={cx('from')}>FPT</div>
+              <div className={cx('name')}>Công ty FPT</div>
               <div className={cx('type')}>IT</div>
             </div>
           </div>
@@ -96,6 +100,7 @@ function Home() {
             </div>
           </div>
 
+  
         </div>
 
         <div className={cx('buttons')}>
@@ -113,6 +118,7 @@ function Home() {
     <div className={cx('introduce')}>
     <div className={cx('introduce-banner')}>
       <h1>Give me your money</h1>
+      <img src={bannerimages} className={cx('banner')}/>
     </div>
     </div>
 
